6f015bd19f8b6ef7cf3b50ae4a6c724637f66300
liyaxuxue <1758626770@qq.com>
2019-06-19 09:10:17 +0800

    第一次提交

---
+++ pages/index/index.js
@@ -0,0 +1,103 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    aessage:"js数据",
+    slidesList:[],
+    navList:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.request({
+      url: 'https://locally.uieee.com/slides',
+      success:(res)=>{
+        // console.log(res)
+        // 1、设置页面的data数据
+        // console.log(this.data)
+        // this.data.slidesList = res.data;不像其他的可以双向数据绑定 这是赋值
+        /**
+         *  this.setData有两个功能；
+         * 1、更新数据
+         * 2、更新视图
+         */
+        this.setData({
+          slidesList:res.data
+        })
+       
+      }
+    });
+    wx.request({
+      url: 'https://locally.uieee.com/categories',
+      success: (res) => {
+        // console.log(res)
+        // 1、设置页面的data数据
+        // console.log(this.data)
+        // this.data.slidesList = res.data;不像其他的可以双向数据绑定 这是赋值
+        /**
+         *  this.setData有两个功能；
+         * 1、更新数据
+         * 2、更新视图
+         */
+        this.setData({
+          navList: res.data
+        })
+
+      }
+    });
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
---
+++ pages/yikaishi/huiyigaiyao.js
@@ -0,0 +1,138 @@
+// pages/yikaishi/huiyigaiyao.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    startpx: '',
+    curCell: '999',
+    data: [1],
+    curIndex: '',
+
+    condition:true,
+    currentTab: 0,
+  },
+  touchstart: function (e) {
+    // console.log(e.changedTouches[0].pageX)
+    const index = e.currentTarget.dataset.index;
+    this.setData({
+      startpx: e.changedTouches[0].pageX,
+      curIndex: index
+    })
+  },
+  touchmove: function (e) {
+    // console.log(e.changedTouches[0].pageX)
+    const mpx = e.changedTouches[0].pageX;
+    const curIndex = this.data.curIndex;
+    if (mpx - this.data.startpx < 0) {
+      this.setData({
+        curCell: curIndex
+      })
+    } else {
+      this.setData({
+        curCell: '999'
+      })
+    }
+  },
+
+  // jianjie: function () {
+  //   // console.log('xixiix')
+  //   this.setData({
+  //     condition: true,
+  //     currentTab: 0
+  //   })
+  // },
+  // pinglun: function () {
+  //   // console.log('xixiix')
+  //   this.setData({
+  //     condition: false,
+  //     currentTab: 1
+  //   })
+  // },
+
+  // pinglun: function () {
+  //   console.log('xixiix')
+  //   this.setData({
+  //     condition: false,
+  //     currentTab: 2
+  //   })
+  // },
+  
+  swichNav:function(e){
+    // 
+    var that = this;
+    console.log(this.data.currentTab)
+    console.log(e.target.dataset.current)
+    if (this.data.currentTab === e.target.dataset.current){
+      return false;
+    }else{
+      that.setData({
+        condition: false,
+        currentTab: e.target.dataset.current
+        
+      })
+
+    }
+  },
+  
+  touchend: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
